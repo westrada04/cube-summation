e23aa2cc8af82d472d784e46e010bdc1708a5c7f
Wilmer Estrada Diaz <westrada@softtek.com>
2018-01-04 15:31:17 -0500

    Actualizacion

--- app/Http/Controllers/CuboController.php
+++ app/Http/Controllers/CuboController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class CuboController extends Controller
 {
-    //
+    public function index(){
+        return view('main.main');
+    }
+    
+    public function configurar(Request $request){
+        $datos = new Datos();
+        $this->validate($request, [
+            'numeroMatriz' => 'required|integer',
+            'numeroComandos' => 'required|integer'
+        ]);
+
+        $input = $request->only('numeroComandos', 'numeroMatriz');
+        $matriz = new Matriz($input['numeroMatriz'], $input['numeroComandos']);
+
+        $datos->setMatriz($matriz);
+        $datos->setAcciones(0);
+
+        return response()->json(['success' => 'true']);
+
+    }
 }
