97bd851a6aceaebac604f0ca90d3a1127afd4e54
Wilmer Estrada Diaz <westrada@softtek.com>
2018-01-04 15:13:10 -0500

    Actualizacion

--- app/Datos.php
+++ app/Datos.php
@@ -9,5 +9,38 @@
 namespace App;
 
 class Datos{
-    
+    public function __construct(){
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+    }
+
+    public function setAcciones($actions){
+        $_SESSION['actions'] = $actions;
+    }
+
+    public function getAcciones(){
+        return $this->retornarValor('actions');
+
+    }
+
+    public function getMatriz(){
+        return $this->retornarValor('matriz');
+    }
+
+    public function setMatriz($matriz){
+        $_SESSION['matriz'] = $matriz;
+    }
+
+    public function deleteSession(){
+        session_destroy();
+    }
+
+    private function retornarValor($key){
+        $value = null;
+        if (isset($_SESSION[$key])) {
+            $value = $_SESSION[$key];
+        }
+        return $value;
+    }
 }
--- app/Matriz.php
+++ app/Matriz.php
@@ -9,5 +9,51 @@
 namespace App;
 
 class Matriz{
+    private $numeroComandos;
+    private $numeroMatriz
+    private $matriz;
 
+    function __construct($numeroMatriz, $numeroComandos){
+        $this->numeroComandos = $numeroComandos;
+        $this->numeroMatriz = $numeroMatriz;
+        $this->construirMatriz($numeroMatriz);
+    }
+
+    private function construirMatriz($numeroMatriz){
+        for ($i = 0; $i <= $numeroMatriz; $i++) {
+            for ($j = 0; $j <= $numeroMatriz; $j++) {
+                for ($k = 0; $k <= $numeroMatriz; $k++) {
+                    $this->matriz[$i][$j][$k] = 0;
+                }
+            }
+        }
+    }
+
+    public function actualizarValor($x, $y, $z, $value){
+        $this->matriz[$x][$y][$z] = $value;
+    }
+
+    public function query($x1, $y1, $z1, $x2, $y2, $z2){
+        $sum = 0;
+        for ($i = $x1; $i <= $x2; $i++) {
+            for ($j = $y1; $j <= $y2; $j++) {
+                for ($k = $z1; $k <= $z2; $k++) {
+                    $sum += $this->matriz[$i][$j][$k];
+                }
+            }
+        }
+        return $sum;
+    }
+
+    public function getNumeroComandos(){
+        return $this->numeroComandos;
+    }
+
+    public function getMatriz(){
+        return $this->matriz;
+    }
+
+    public function getNumeroMatriz(){
+        return $this->numeroMatriz;
+    }
 }
